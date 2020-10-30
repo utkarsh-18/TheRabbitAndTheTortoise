2f7c873cd7bb9ffbf6ce6e06c8f11cbba44b1d63
Utkarsh Ojha <68592956+utkarsh-18@users.noreply.github.com>
2020-10-30 21:49:17 +0530

    Update Race.java

--- src/main/Race.java
+++ src/main/Race.java
@@ -6,5 +6,7 @@ public class Race {
     public static void main(String[] args) {
         Rabbit rabbit = new Rabbit();
         rabbit.getRabbit().start();
+        Tortoise tortoise = new Tortoise();
+        tortoise.start();
     }
 }
